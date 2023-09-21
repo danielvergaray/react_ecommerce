c77618a6e7d313368885e495506532941ddfcb9a
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-09-21 16:20:02 -0500

    intento de convertir el fetch en un custome hook

---
+++ src/components/customHooks/useApiData.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+const useApiData = (url, page) => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+    /* const [page, setPage] = useState(8); */
+
+    const getProducts = () => {
+
+
+        fetch(url)
+            .then((infoApi) => infoApi.json())
+            .then((infoApiJson) => setProducts(infoApiJson))
+            .catch((error) => setError(true))
+            .finally(() => {
+                setLoading(false)
+            })
+
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            getProducts(products)
+        }, 1000)
+        /* if (page === 20) {
+            setBtnMas(false)
+        } */
+    }, [url, page])
+
+    
+    return [loading, products, error]
+
+}
+
+
+
+export default useApiData
--- src/components/item/ItemListContainer.jsx
+++ src/components/item/ItemListContainer.jsx
@@ -1,48 +1,53 @@
 import React, { useEffect, useState } from 'react'
 import ItemList from './ItemList';
 import CustomSpinner from '../reutilizables/Spinner';
+import useApiData from '../customHooks/useApiData';
 
 
 const ItemListContainer = () => {
 
 
-  const [products, setProducts] = useState([]);
+  /* const [products, setProducts] = useState([]); */
   const [page, setPage] = useState(8);
-  const [loading, setLoading] = useState(true)
-  const [btnMas, setBtnMas]= useState(false)
+  /* const [loading, setLoading] = useState(true) */
+  const [btnMas, setBtnMas] = useState(true)
 
 
-  const getProducts = () => {
+  const [items, products, loading, error] = useApiData(`https://fakestoreapi.com/products?limit=${page}`)
+
+  /* const getProducts = () => {
     fetch(`https://fakestoreapi.com/products?limit=${page}`)
       .then((infoApi) => infoApi.json())
       .then((infoApiJson) => setProducts(infoApiJson))
       .catch((error) => console.log(error))
       .finally(() => {
         setLoading(false)
-        setBtnMas(true)
       })
 
-  }
+  } */
 
   useEffect(() => {
-    setLoading(true)
-    setBtnMas(!btnMas)
+    /* setLoading(true)
     setTimeout(() => {
       getProducts(products)
-    }, 1000)
-
+    }, 1000) */
+    if (page === 20) {
+      setBtnMas(false)
+    }
   }, [page])
 
 
 
   const verMas = () => {
-    if(page<17){
+    if (page < 17) {
       setPage(page + 4);
     } else {
       setBtnMas(false)
       return
     }
+
   }
+
 
 
 
@@ -50,18 +55,18 @@ const ItemListContainer = () => {
   return (
     <div>
 
-      
-      <ItemList
-        productos={products}
-        verMas={verMas}
-        btnMas= {btnMas}
+      {items !== null &&
+        <ItemList
+          productos={products}
+          verMas={verMas}
+          btnMas={btnMas}
 
 
-      />
+        />}
       {loading && <CustomSpinner
-      animation= "border"
-      message="Cargando..."
-      
+        animation="border"
+        message="Cargando..."
+
       />}
     </div>
 
